53841dca52c0980787297353a9a8899d5c39cc92
juancastro000 <cdyutub@gmail.com>
2025-03-11 22:51:07 +0100

    post category was implemented and jakarta dependency was added

--- src/main/java/dev/proyect6/codehub/codehub/controllers/CategoryController.java
+++ src/main/java/dev/proyect6/codehub/codehub/controllers/CategoryController.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.proyect6.codehub.codehub.models.Category;
 import dev.proyect6.codehub.codehub.services.CategoryService;
+import jakarta.validation.Valid;
 
 
 @RestController
 @RequestMapping("/categories")
+@Validated
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
@@ -29,5 +34,10 @@ public class CategoryController {
         return categoryService.getCategoryById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createCategory(@Valid @RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.saveCategory(category));
     }
 }
