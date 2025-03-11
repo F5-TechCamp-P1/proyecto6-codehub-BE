8820a557b6c2e8ace5786ae50a42677980d7f594
juancastro000 <cdyutub@gmail.com>
2025-03-11 22:34:49 +0100

    endpoint get all categories and get categories by id implemented

--- src/main/java/dev/proyect6/codehub/codehub/controllers/CategoryController.java
+++ src/main/java/dev/proyect6/codehub/codehub/controllers/CategoryController.java
@@ -1,5 +1,33 @@
 package dev.proyect6.codehub.codehub.controllers;
 
-public class CategoryController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.proyect6.codehub.codehub.models.Category;
+import dev.proyect6.codehub.codehub.services.CategoryService;
+
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+     @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
