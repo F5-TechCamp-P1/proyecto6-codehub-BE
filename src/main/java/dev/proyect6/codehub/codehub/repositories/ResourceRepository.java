da82254ff3b0fa22481f377051df8bfd33dff901
juancastro000 <cdyutub@gmail.com>
2025-03-11 14:19:48 +0100

    implemented repository for category and resource

--- src/main/java/dev/proyect6/codehub/codehub/repositories/ResourceRepository.java
+++ src/main/java/dev/proyect6/codehub/codehub/repositories/ResourceRepository.java
@@ -2,9 +2,9 @@ package dev.proyect6.codehub.codehub.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import dev.proyect6.codehub.codehub.models.Category;
+import dev.proyect6.codehub.codehub.models.Resource;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
 }
