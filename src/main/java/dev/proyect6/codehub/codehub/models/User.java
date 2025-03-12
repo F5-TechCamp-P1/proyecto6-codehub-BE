c3a63111f554eff56eed6b99071c806f5edb313c
NaudelynLucena <naulunau@gmail.com>
2025-03-12 22:07:50 +0100

    model user created

---
+++ src/main/java/dev/proyect6/codehub/codehub/models/User.java
@@ -0,0 +1,30 @@
+package dev.proyect6.codehub.codehub.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
