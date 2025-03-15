c87fb0cda673e281baa671fb01f921b3767959a0
juancastro000 <cdyutub@gmail.com>
2025-03-15 19:12:40 +0100

    test resource, categoryService and resourceService completed

--- src/test/java/dev/proyect6/codehub/codehub/models/CategoryTest.java
+++ src/test/java/dev/proyect6/codehub/codehub/models/CategoryTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Set;
 
-class TestCategory {
+class CategoryTest {
 
     private Validator validator;
 
