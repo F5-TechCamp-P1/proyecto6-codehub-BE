6daf31620950f5f08aec7f3e987d5eb7cae4b56a
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-16 18:22:22 +0100

    Test login method ok al 83%

---
+++ src/test/java/dev/proyect6/codehub/codehub/controllers/AuthControllerTest.java
@@ -0,0 +1,78 @@
+package dev.proyect6.codehub.codehub.controllers;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import dev.proyect6.codehub.codehub.models.User;
+import dev.proyect6.codehub.codehub.services.AuthService;
+
+@WebMvcTest(controllers = AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthControllerTest {  
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    AuthService authService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+    
+    @Test
+    @DisplayName("Test login")
+    void testLogin() throws Exception {
+        //GIVEN
+        User credentials = new User();
+        String apiKey = "12345abc";
+
+        User okUser = new User();
+        okUser.setApikey(apiKey);
+        credentials.setUsername("admin");
+        credentials.setPassword("password");
+
+        //WHEN
+        when(authService.authenticate(any(User.class))).thenReturn(Optional.of(okUser));
+        
+        MockHttpServletResponse response = mockMvc
+        .perform(            
+            post("/api/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(credentials))
+            .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isOk())
+        .andReturn()
+        .getResponse();
+        
+        System.out.println(response.getContentAsString());
+        //THEN
+
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getContentAsString(), containsString(apiKey));
+
+    }
+}
