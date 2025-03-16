ab5997d183847a6cae6d70d36d87bb3b22a2af7f
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-16 19:50:06 +0100

    Testing CategoryController 100%

--- src/test/java/dev/proyect6/codehub/codehub/controllers/CategoryControllerTest.java
+++ src/test/java/dev/proyect6/codehub/codehub/controllers/CategoryControllerTest.java
@@ -4,7 +4,9 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -13,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +31,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import dev.proyect6.codehub.codehub.config.AuthFilter;
 import dev.proyect6.codehub.codehub.models.Category;
-import dev.proyect6.codehub.codehub.models.User;
 import dev.proyect6.codehub.codehub.services.CategoryService;
 
 @WebMvcTest(controllers = CategoryController.class)
@@ -107,6 +107,238 @@ public class CategoryControllerTest {
         .getResponse();
 
         //THEN        
+        assertThat(response.getStatus(), is(401));
+        assertThat(response.getContentAsString(), containsString("Apikey incorrecta"));
+    }
+    @Test
+    @DisplayName("Test createCategory")
+    void testCreateCategory() throws Exception {
+
+        //GIVEN
+        Category category = new Category();
+        category.setName("Category 3");
+
+        when(authFilter.preHandle(anyString())).thenReturn(true); 
+        when(categoryService.saveCategory(any(Category.class))).thenReturn(category);
+
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            post("/api/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(category))
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse();
+
+        //THEn
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getContentAsString(), containsString("Category 3"));
+
+    }
+    @Test
+    @DisplayName("Test createCategory with bad apikey")
+    void testCreateCategoryWithBadApikey() throws Exception {
+
+        //GIVEN
+        Category category = new Category();
+        category.setName("Category 3");
+
+        when(authFilter.preHandle(anyString())).thenReturn(false); 
+        when(categoryService.saveCategory(any(Category.class))).thenReturn(category);
+
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            post("/api/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(category))
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isUnauthorized())
+            .andReturn()
+            .getResponse();
+
+        //THEn
+        assertThat(response.getStatus(), is(401));
+        assertThat(response.getContentAsString(), containsString("Apikey incorrecta"));
+    }
+    @Test
+    @DisplayName("Test updateCategory")
+    void testUpdateCategory() throws Exception {
+
+        //GIVEN
+        Category category = categories.get(0);
+        category.setName("Category 3");
+        when(authFilter.preHandle(anyString())).thenReturn(true); 
+        when(categoryService.updateCategory(anyLong(), any(Category.class))).thenReturn(category);
+
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            put("/api/categories/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(category))
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse();
+
+        //THEn
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getContentAsString(), containsString("Category 3"));
+
+    }
+    @Test
+    @DisplayName("Test updateCategory with bad apikey")
+    void testUpdateCategoryWithBadApikey() throws Exception {    
+
+        //GIVEN        
+        Category category = categories.get(0);
+        category.setName("Category 3");
+        when(authFilter.preHandle(anyString())).thenReturn(false); 
+        when(categoryService.updateCategory(anyLong(), any(Category.class))).thenReturn(category);
+
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            put("/api/categories/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(category))
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isUnauthorized())
+            .andReturn()
+            .getResponse();
+
+        //THEN
+        assertThat(response.getStatus(), is(401));
+        assertThat(response.getContentAsString(), containsString("Apikey incorrecta"));
+    }
+    @Test
+    @DisplayName("Test updateCategory with bad id")
+    void testUpdateCategoryWithBadId() throws Exception {
+
+        //GIVEN        
+        Category category = categories.get(0);
+        when(authFilter.preHandle(anyString())).thenReturn(true); 
+        when(categoryService.updateCategory(eq(4L), any(Category.class)))
+            .thenThrow(new RuntimeException("Category not found"));
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            put("/api/categories/4")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(category))
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isNotFound())
+            .andReturn()
+            .getResponse();
+
+        //THEN
+        assertThat(response.getStatus(), is(404));        
+    }
+    @Test
+    @DisplayName("Test deleteCategory")
+    void testDeleteCategory() throws Exception {
+
+        //GIVEN        
+        when(authFilter.preHandle(anyString())).thenReturn(true); 
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            delete("/api/categories/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isOk())            
+            .andReturn()
+            .getResponse();        
+
+
+        //THEN
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getContentAsString(), containsString("Categoría borrada exitosamente"));       
+    }
+    @Test
+    @DisplayName("Test deleteCategory with bad apikey")
+    void testDeleteCategoryWithBadApikey() throws Exception {
+
+        //GIVEN        
+        when(authFilter.preHandle(anyString())).thenReturn(false); 
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            delete("/api/categories/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isUnauthorized())            
+            .andReturn()
+            .getResponse();        
+
+
+        //THEN
+        assertThat(response.getStatus(), is(401));
+        assertThat(response.getContentAsString(), containsString("Apikey incorrecta"));       
+    }
+    @Test
+    @DisplayName("Test getCategoryById")
+    void testGetCategoryById() throws Exception {
+
+        //GIVEN
+        when(authFilter.preHandle(anyString())).thenReturn(true); 
+        when(categoryService.getCategoryById(1L)).thenReturn(Optional.of(categories.get(0)));
+
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            get("/api/categories/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse();
+
+        //THEN
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getContentAsString(), containsString("Category 1"));
+    }
+    
+    @Test
+    @DisplayName("Test getCategoryById with bad apikey")
+    void testGetCategoryByIdWithBadApikey() throws Exception {
+
+        //GIVEN
+        when(authFilter.preHandle(anyString())).thenReturn(false);                
+        when(categoryService.getCategoryById(1L)).thenReturn(Optional.of(categories.get(0)));
+
+        //WHEN
+        MockHttpServletResponse response = mockMvc.perform(
+            get("/api/categories/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-API-KEY", "12345abc")
+                .accept(MediaType.APPLICATION_JSON)
+
+            )
+            .andExpect(status().isUnauthorized())
+            .andReturn()
+            .getResponse();
+
+        //THEN
         assertThat(response.getStatus(), is(401));
         assertThat(response.getContentAsString(), containsString("Apikey incorrecta"));
     }
